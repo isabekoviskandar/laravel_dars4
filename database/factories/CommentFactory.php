e40fc8723d896b1961fadda960d5759d6d53aacc
Kenc1k <iskandarisabrkov@gmail.com>
2024-10-24 13:46:51 +0500

    "fake malumotlar qoshish"

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'post_id' => rand(1,10),
+            'body' =>$this->faker->text(100),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
+// Route::get('/category' , [CategoryController::class, 'index']);
+
